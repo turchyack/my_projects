e5ebaa6344c3439ae6a9da4af1db57d28fed15de
turchyack <turchyack@gmail.com>
2022-07-03 01:48:41 +0300

    первый коммит

---
+++ newserv/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include <map>
+using namespace std;
+
+
+int main(){
+
+printf("g11\n123123\n1111");
+
+printf("g11\n123123\n1111");
+
+printf("g11\n123123\n1111");
+
+printf("g11\n123123\n1111");
+
+
+
+
+}
